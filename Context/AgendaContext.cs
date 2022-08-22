47edeb48edad1152370d4036d393d1fcc8802613
ozanamsousa <ozanamsousa@gmail.com>
2022-08-22 15:14:28 -0300

    Trabalhando com crud

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoMvc.Models;
+
+namespace ProjetoMvc.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+       {
+
+       }
+       public DbSet<Contato> Contatos { get; set; } 
+    }
+}
